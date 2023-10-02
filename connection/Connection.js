7a1279affc44d8b21ebd6660481811eccae51aa5
Goutham <gouthamkumarpolapally@gmail.com>
2023-10-02 12:57:05 +0530

    worked on Mongoose DeprecatedWarning

--- connection/Connection.js
+++ connection/Connection.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-
+mongoose.set('strictQuery', false);
 mongoose.connect("mongodb+srv://CompeteTricks:CompeteTricks@cluster0.qjay0vb.mongodb.net/?retryWrites=true&w=majority").then((data)=>{console.log("Connected to Database")}).catch((err)=>{
     console.log("Connection Error while Connecting to db"+err.message);
 });
