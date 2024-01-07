9f9b717ba8bb164a59e6b114d3e2d515d1263d88
Goutham <gouthamkumarpolapally@gmail.com>
2024-01-07 18:56:04 +0530

    cors added

--- app.js
+++ app.js
@@ -3,12 +3,13 @@ const app = express();
 const dotenv = require("dotenv");
 dotenv.config({path : "./.env"});
 const port =  process.env.PORT || 5000 ;
+const cors = require("cors");
 const Connection = require("./connection/Connection");
 const Auth = require("./routes/Auth");
 const QuizRouter = require("./routes/QuizRouter")
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
-
+app.use(cors());
 app.use("/Auth",Auth);
 app.use("/api/Quiz/upload",QuizRouter);
 app.listen(port,()=>{console.log("Server Running on port : "+port)})
--- routes/Exams.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express();
-const ExamController = require("../controllers/ExamsController");
-
-router.post("/createNewExam", ExamController.CreateNewExam);
-router.get("/getCurrentWeekExams",ExamController.CurrentWeekExams)
-router.delete("/deleteExam/:examId", ExamController.DeleteExam);
-
-module.exports = router;
