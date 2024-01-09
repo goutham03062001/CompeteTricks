fbc352a894356f50e11aca0423c76f38cc9598a7
Goutham <gouthamkumarpolapally@gmail.com>
2024-01-09 16:48:20 +0530

    find bug in update quiz router

--- routes/QuizRouter.js
+++ routes/QuizRouter.js
@@ -5,7 +5,7 @@ const storage = multer.memoryStorage();
 const upload = multer({ storage });
 const QuizController = require("../controllers/QuizController")
 router.post("/uploadQuiz",upload.single("file"),QuizController.upload);
-router.put("/updateAttempts/user/:userId/quiz:quizId",QuizController.updateQuizAttempt);
+router.put("/updateAttempts/user/:userId/quiz/:quizId",QuizController.updateQuizAttempt);
 router.get("/getQuizDetails",QuizController.getAllQuiz);
 router.get("/getQuizDetails/:quizId",QuizController.getQuizDetailsById);
 router.delete("/removeQuiz/:quizId",QuizController.removeQuizById)
