84ebb2c5e5a0a2f1dd0178c908c0916c6ec6f7ec
Goutham <gouthamkumarpolapally@gmail.com>
2024-01-14 13:06:21 +0530

    webhooks router updated

--- routes/Payment.js
+++ routes/Payment.js
@@ -26,19 +26,19 @@ router.post("/create-payment-intent",async(req,res)=>{
         return res.send(error)
     }
 })
-router.post('/webhook', express.raw({type: 'application/json'}), (request, response) => {
-    const sig = request.headers['stripe-signature'];
-    const {userId} = request.body;
+router.post('/webhook', express.raw({type: 'application/json'}), (req, res) => {
+    const sig = req.headers['stripe-signature'];
+    const {userId} = req.body;
     let event;
   
     try {
-      event = stripe.webhooks.constructEvent(request.body, sig, endpointSecret);
+      event = stripe.webhooks.constructEvent(req.body, sig, endpointSecret);
     } catch (err) {
-      response.status(400).send(`Webhook Error: ${err.message}`);
+      res.status(400).send(`Webhook Error: ${err.message}`);
       return;
     }
     async function addAuthorizedUser(userId){
-        let res = response;
+        
         const currentDate = new Date().toLocaleDateString();
         const expireDate = new Date(currentDate).toLocaleDateString();
         expireDate.setFullYear(currentDate.getFullYear() + 1);
@@ -50,7 +50,8 @@ router.post('/webhook', express.raw({type: 'application/json'}), (request, respo
     switch (event.type) {
         case 'payment_intent.created':const paymentIntentCreated = event.data.object;
         console.log("Payment created");
-        // return response.send("Error Occurred "+)
+        return res.send("Payment Intent Created");
+      
         break;
       case 'payment_intent.succeeded':
         const paymentIntentSucceeded = event.data.object;
@@ -61,15 +62,15 @@ router.post('/webhook', express.raw({type: 'application/json'}), (request, respo
         break;
         case 'payment_intent.failed':const paymentIntentFailed = event.data.object;
         console.log("Payment Failed");
-        return response.send("Error Occurred - "+paymentIntentFailed)
+        return res.send("Error Occurred - "+paymentIntentFailed)
         break;
       // ... handle other event types
       default:
         console.log(`Unhandled event type ${event.type}`);
-        return response.send("Error Occurred");
+        return res.send("Error Occurred");
     }
   
-    // Return a 200 response to acknowledge receipt of the event
-    // return response.status(200).send({ok:true})
+    // Return a 200 res to acknowledge receipt of the event
+    // return res.status(200).send({ok:true})
   });
 module.exports = router;
