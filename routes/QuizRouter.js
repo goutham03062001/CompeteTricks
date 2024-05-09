edb830db23561f8f61cad19b89efea85a89b18ff
goutham03062001 <66135872+goutham03062001@users.noreply.github.com>
2024-05-09 23:50:02 +0530

    Update QuizRouter.js
    English Pedagogy Index API added

--- routes/QuizRouter.js
+++ routes/QuizRouter.js
@@ -24,6 +24,7 @@ router.get("/getAllGeneralEnglishPapers",QuizController.getAllGeneralEnglishPape
 router.get("/getGeneralEnglishPaperById/:id",QuizController.getGeneralEnglishPaperById);
 router.delete("/deleteEnglishPedagogyById/:id",QuizController.deleteEnglishPedagogyById);
 router.delete("/deleteGeneralEnglishPaperById/:id",QuizController.deleteGeneralEnglishPaperById);
+router.get("/getEnglishPedagogyIndex/:quizId",QuizController.getEnglishPedagogyPaperIndex);
 router.get('/check/:quizId',async(req,res)=>{
     try {
         const allQuizzes = await QuizModel.find();
